67dc4812ff4f5c11cee4a2b9264b390dbede6871
Aaron France <aaron.l.france@gmail.com>
2014-01-25 17:40:48 +0100

    Change project version.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject awizo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject awizo "0.0.1"
+  :description "Filesystem events library."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
