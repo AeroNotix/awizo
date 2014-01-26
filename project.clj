96b99c7de4c0c0ad83e127950094ec2f307902d4
Aaron France <aaron.l.france@gmail.com>
2014-01-27 00:43:46 +0100

    WIP: Use timers and agents in order to not smash thread-pools.
    If we do not control the concurrency of filewatchers then it means
    clients will need to control it. They cannot just spawn off goroutines
    and hope for the best since the handler calls block.
    
    By employing agents and timers I hope to be able to simply provide a
    channel from which the client can block on.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject awizo "0.0.2"
+(defproject awizo "0.0.3"
   :description "Filesystem events library."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]])
--- src/awizo/core.clj
+++ src/awizo/core.clj
@@ -1,10 +1,34 @@
 (ns awizo.core
+  (:require [clojure.core.async :as async])
   (:import [java.nio.file StandardWatchEventKinds])
-  (:import [java.nio.file WatchEvent$Kind]))
+  (:import [java.nio.file WatchEvent$Kind])
+  (:import [java.util Timer])
+  (:import [java.util TimerTask]))
+
+
+(def watch->chan (atom {}))
+(def timer       (agent (Timer.)))
+(def periodicity (long 500))
 
 (def CREATE StandardWatchEventKinds/ENTRY_CREATE)
 (def DELETE StandardWatchEventKinds/ENTRY_DELETE)
 (def MODIFY StandardWatchEventKinds/ENTRY_MODIFY)
+
+
+(defn schedule-task [task]
+  (send-off timer (fn [t]
+                    (.scheduleAtFixedRate t task periodicity periodicity))))
+
+(defn poll [watch c]
+  (if-let [watch-key (.poll watch)]
+    (do
+      (doseq [event (.pollEvents watch-key)]
+        (async/go
+         (async/>! c event)))
+      (.reset watch-key))))
+
+(defn close [c]
+  )
 
 (defn string->path [p]
   (.toPath (clojure.java.io/file p)))
@@ -16,12 +40,14 @@
   (into-array WatchEvent$Kind events))
 
 (defn attach-handler [p handler event-types]
-  (let [path (string->path p)
+  (let [c     (async/chan)
+        path  (string->path p)
         watch (path->watch path)
         events (seq->event-array event-types)]
     (.register path watch events)
-    (while true
-      (let [watch-key (.take watch)]
-        (doseq [event (.pollEvents watch-key)]
-          (handler event))
-        (.reset watch-key)))))
+    (swap! watch->chan assoc c watch)
+    (let [task (proxy [TimerTask] []
+                 (run []
+                   (poll watch c)))]
+      (schedule-task task))
+    c))
