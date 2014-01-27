100dbfe4a6596fba4f669493362f1edca4e30372
Aaron France <aaron.l.france@gmail.com>
2014-01-27 01:04:12 +0100

    Print the stacktrace on agent errors.

--- src/awizo/core.clj
+++ src/awizo/core.clj
@@ -7,7 +7,12 @@
 
 
 (def watch->chan (atom {}))
+
+(defn timer-error-handler [timer ex]
+  (doseq [ste (.getStackTrace ex)]
+    (println ste)))
 (def timer       (agent (Timer.)))
+(set-error-handler! timer timer-error-handler)
 (def periodicity (long 500))
 
 (def CREATE StandardWatchEventKinds/ENTRY_CREATE)
