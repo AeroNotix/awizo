badceb42d1c561cedd7e25b8ddb36ecd0b4ff4c5
Aaron France <aaron.l.france@gmail.com>
2014-06-18 16:29:30 +0200

    Add events-smoother.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject awizo "0.0.3"
+(defproject awizo "0.0.4"
   :description "Filesystem events library."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]])
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.cache "0.6.3"]])
--- src/awizo/core.clj
+++ src/awizo/core.clj
@@ -1,5 +1,6 @@
 (ns awizo.core
-  (:require [clojure.core.async :as async])
+  (:require [clojure.core.async :as async]
+            [clojure.core.cache :as cache])
   (:import [java.nio.file StandardWatchEventKinds])
   (:import [java.nio.file WatchEvent$Kind])
   (:import [java.util Timer])
@@ -13,10 +14,20 @@
 (set-error-handler! timer timer-error-handler)
 (def periodicity (long 5000))
 
+(def events-smoother (agent (cache/ttl-cache-factory {} :ttl 1000)))
+
 (def CREATE StandardWatchEventKinds/ENTRY_CREATE)
 (def DELETE StandardWatchEventKinds/ENTRY_DELETE)
 (def MODIFY StandardWatchEventKinds/ENTRY_MODIFY)
 
+(defn no-recent-events? [uref]
+  (cache/has? @events-smoother uref))
+
+(defn set-recent-event! [uref]
+  (send events-smoother assoc uref true))
+
+(defn make-ref []
+  (. clojure.lang.RT (nextID)))
 
 (defn schedule-task [task]
   (send-off
@@ -46,14 +57,18 @@
   (let [c      (async/chan)
         path   (string->path p)
         watch  (path->watch path)
-        events (seq->event-array event-types)]
+        events (seq->event-array event-types)
+        uref   (make-ref)]
     (.register path watch events)
     (let [task (proxy [TimerTask] []
                  (run []
-                   (poll watch c)))]
+                   (when (no-recent-events? uref)
+                     (do
+                       (set-recent-event! uref)
+                       (poll watch c)))))]
       (schedule-task task))
     (async/go-loop
-     [e (async/<! c)]
-     (handler e)
-     (recur (async/<! c))))
+        [e (async/<! c)]
+      (handler e)
+      (recur (async/<! c))))
   nil)
